734db38b56d4a7dd12b00d269a1877d0fe47fc47
alireza <alirezashateri7@gmail.com>
2020-10-12 00:40:10 +0330

    Add error handler to Scanner class

--- lexer.py
+++ lexer.py
@@ -12,21 +12,27 @@ class Token:
 
 
 class Table:
-    table = {}
-    pass
+
+    def log(self, *args, **kwargs):
+        raise NotImplementedError("Method not implemented")
 
     def write_on_file(self):
         raise NotImplementedError("Method not implemented")
 
 
 class ErrorTable(Table):
+    TEMPLATE = 'Error in line {line_number}: {msg}'
 
     def __init__(self, file_name='lexical_errors', extension='txt'):
         self.file_name = file_name
         self.extension = extension
+        self.id = 0
+        self.errors = []
 
-    def generate_error(self, description):
-        pass
+    def log(self, exception, line_number):
+        message = self.TEMPLATE.format(line_number=line_number,
+                                       msg=exception.message)
+        self.errors.append(message)
 
     def write_on_file(self):
         pass
@@ -38,6 +44,9 @@ class SymbolTable(Table):
         self.__identifiers = {}
         self.id = 0
         self.__init()
+
+    def log(self):
+        pass
 
     def write_on_file(self):
         pass
@@ -59,6 +68,9 @@ class SymbolTable(Table):
 
 
 class TokenTable(Table):
+
+    def log(self):
+        pass
 
     def write_on_file(self):
         pass
@@ -103,7 +115,17 @@ class Scanner:
         self.dfa = DFA()
         self.line_number = 1
 
-    def get_next_token(self, ):
+        self.error_table = ErrorTable()
+        self.symbol_table = SymbolTable()
+        self.token_table = TokenTable()
+
+    def get_next_token(self):
+        try:
+            self.__get_next_token()
+        except RegexNotMatchError as e:
+            self.error_table.log(e, self.line_number)
+
+    def __get_next_token(self, ):
         while True:
             try:
                 char = self.buffer()
